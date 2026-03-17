a6aaea596e22849b09bbf3264a96320baaa031e1
sidsahay <ssahay2@andrew.cmu.edu>
2026-03-17 19:19:26 -0400

    Configure base path for archviz

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // Specify the base path for your GitHub Pages repository.
   // For example, if you host at username.github.io/sv-simulator/, this must be '/sv-simulator/'
-  // If you host at a custom domain or the root username.github.io, you can comment this out or set it to '/'
-  base: '/sv-simulator/',
+  base: '/archviz/',
 })
